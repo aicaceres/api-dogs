fe8f51e3e78ccab6cac8877c4d3e7a03e80ffcdb
Alejandra Cáceres <alejandraicaceres@gmail.com>
2022-10-17 09:52:30 -0300

    sync sequelize to false

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(process.env.PORT, function() {
     console.log('listening at %s',this.address().port); // eslint-disable-line no-console
   });
